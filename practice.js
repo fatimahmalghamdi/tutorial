8448b255b1f3b93c405aa6c038fceb1ded7a9712
Fatimah Alghamdi <fatimah-ma@hotmail.com>
2022-05-28 04:18:40 +0300

    firstfileuploadme

---
+++ practice.js
@@ -0,0 +1,24 @@
+
+
+var str2="Hello";
+var str1="fatimah";
+var nn=32;
+var num1=0;
+
+var arr = [1,2,3,4,5,6,7,8,9,10];
+var arr2 = ["Fatimah","Arwa","Raghad"];
+
+
+console.log(str2+" "+str1);
+console.log("the value of nn= "+nn);
+
+//while (num1<=5)
+//{
+  //  console.log(num1);
+   // ++num1;
+//}
+
+arr.push(32);
+console.log(arr);
+console.log(arr.length);
+console.log(arr.length);
